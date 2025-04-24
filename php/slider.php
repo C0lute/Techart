90a05b504f28f6302f417e4e82ecfd87e06fc4f7
C0lute <ilya.starikov.1990@mail.ru>
2025-04-24 17:24:05 +0300

    change in sliders.php; first.js

--- php/slider.php
+++ php/slider.php
@@ -7,12 +7,6 @@ include "./bd.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
-    />
-
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <title>Слайдер</title>
 </head>
 <body>
@@ -35,30 +29,7 @@ include "./bd.php";
     
         </div>
     </div>
-    <script>
-        const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        // direction: 'vertical',
-        // loop: false,
-
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-        slidesPerView: 2,
-        
-        });
+    <script src="../webpack/dist/new.js">
     </script>
 </body>
 </html>
